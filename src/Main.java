35b337a2a06a3a94c3debb4c667427b51e4d15f7
Иванов Антон <ivanovvlg13@yandex.ru>
2022-09-25 21:35:57 +0300

    добавлены новые задания из практики

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,18 @@ public class Main {
         double d = 100000000.99999999999;
         float f = 10000.9999999F;
         boolean q = true;
+        //шестнадцатиричная система счисления
+        int w = 0x00;
+        int e = 0x01;
+        int y = 0x0A;
+        int t = 0x0B;
+        //двоичная система счисления
+        int ab1 = 0b0;
+        int ab2 = 0b1;
+        //переменная char
+        char ch = 'g';
+        char ab = '\"';
+
 
 
         System.out.println( u);
@@ -17,6 +29,14 @@ public class Main {
         System.out.println( d);
         System.out.println( f);
         System.out.println( q);
+        System.out.println( w);
+        System.out.println( e);
+        System.out.println( y);
+        System.out.println( t);
+        System.out.println( ab1);
+        System.out.println( ab2);
+        System.out.println( ch);
+        System.out.println( "Hello World" + ab);
     }
 
 
