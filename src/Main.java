17144b9b15376da112296ccfd58d8de822cdfc86
Иванов Антон <ivanovvlg13@yandex.ru>
2022-09-25 18:15:14 +0300

    созданы 8 переменных, присвоены значения и выведены в консоль

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte u = 100;
+        short i = 32000;
+        int a = 2000000;
+        long r = 2100000;
+        double d = 100000000.99999999999;
+        float f = 10000.9999999F;
+        boolean q = true;
+
+
+        System.out.println( u);
+        System.out.println( i);
+        System.out.println( a);
+        System.out.println( r);
+        System.out.println( d);
+        System.out.println( f);
+        System.out.println( q);
+    }
+
+
+
+
+}
